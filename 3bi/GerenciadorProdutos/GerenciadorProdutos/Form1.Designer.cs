bb0ff488e5a9e0bd5a74a933497d764231083e0a
GEOVANNA SILVA COSTA <geovanna.costa@e239.local>
2026-09-10 14:40:33 -0300

    Gerenciador de produtos

---
+++ 3bi/GerenciadorProdutos/GerenciadorProdutos/Form1.Designer.cs
@@ -0,0 +1,164 @@
+﻿namespace GerenciadorProdutos
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.txtPrecoBase = new System.Windows.Forms.TextBox();
+            this.cmbTipo = new System.Windows.Forms.ComboBox();
+            this.txtAtributoEspecifico = new System.Windows.Forms.TextBox();
+            this.lblAtributo = new System.Windows.Forms.Label();
+            this.btnSalvar = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtNome
+            // 
+            this.txtNome.Location = new System.Drawing.Point(258, 50);
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(100, 20);
+            this.txtNome.TabIndex = 0;
+            // 
+            // txtPrecoBase
+            // 
+            this.txtPrecoBase.Location = new System.Drawing.Point(258, 116);
+            this.txtPrecoBase.Name = "txtPrecoBase";
+            this.txtPrecoBase.Size = new System.Drawing.Size(100, 20);
+            this.txtPrecoBase.TabIndex = 1;
+            this.txtPrecoBase.TextChanged += new System.EventHandler(this.txtPrecoBase_TextChanged);
+            // 
+            // cmbTipo
+            // 
+            this.cmbTipo.FormattingEnabled = true;
+            this.cmbTipo.Location = new System.Drawing.Point(258, 196);
+            this.cmbTipo.Name = "cmbTipo";
+            this.cmbTipo.Size = new System.Drawing.Size(121, 21);
+            this.cmbTipo.TabIndex = 2;
+            // 
+            // txtAtributoEspecifico
+            // 
+            this.txtAtributoEspecifico.Location = new System.Drawing.Point(258, 286);
+            this.txtAtributoEspecifico.Name = "txtAtributoEspecifico";
+            this.txtAtributoEspecifico.Size = new System.Drawing.Size(100, 20);
+            this.txtAtributoEspecifico.TabIndex = 3;
+            // 
+            // lblAtributo
+            // 
+            this.lblAtributo.AutoSize = true;
+            this.lblAtributo.Location = new System.Drawing.Point(388, 289);
+            this.lblAtributo.Name = "lblAtributo";
+            this.lblAtributo.Size = new System.Drawing.Size(43, 13);
+            this.lblAtributo.TabIndex = 4;
+            this.lblAtributo.Text = "Atributo";
+            // 
+            // btnSalvar
+            // 
+            this.btnSalvar.Location = new System.Drawing.Point(264, 364);
+            this.btnSalvar.Name = "btnSalvar";
+            this.btnSalvar.Size = new System.Drawing.Size(75, 23);
+            this.btnSalvar.TabIndex = 5;
+            this.btnSalvar.Text = "Salvar";
+            this.btnSalvar.UseVisualStyleBackColor = true;
+            this.btnSalvar.Click += new System.EventHandler(this.btnSalvar_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(258, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Nome:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(258, 97);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(62, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "PrecoBase:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(261, 165);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(31, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Tipo:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(255, 260);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(102, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Atríbuto Específico:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnSalvar);
+            this.Controls.Add(this.lblAtributo);
+            this.Controls.Add(this.txtAtributoEspecifico);
+            this.Controls.Add(this.cmbTipo);
+            this.Controls.Add(this.txtPrecoBase);
+            this.Controls.Add(this.txtNome);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtNome;
+        private System.Windows.Forms.TextBox txtPrecoBase;
+        private System.Windows.Forms.ComboBox cmbTipo;
+        private System.Windows.Forms.TextBox txtAtributoEspecifico;
+        private System.Windows.Forms.Label lblAtributo;
+        private System.Windows.Forms.Button btnSalvar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ 3bi/GerenciadorProdutos/GerenciadorProdutos/Produto.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GerenciadorProdutos
+{
+    public abstract class Produto
+    {
+        // Encapsulamento através de propiedades
+        public string Id { get; set; }
+        public string Nome { get; set; }
+        public decimal PrecoBase { get; set; }
+
+        //Construtor de classe base
+        protected Produto(string nome, decimal precoBase)
+        {
+            Nome = nome;
+            PrecoBase = precoBase;
+        }
+
+        //Método abstrato que força a subclasse a implementar o polimorfismo
+        public abstract decimal CalcularPrecoFinal();
+    }
+}
